439c7c736e26a03cf5d3b7900c42b0a883b55294
smohan <shiwali.mohan@parc.com>
2017-03-23 11:51:12 -0700

    An example of writing an input to Soar from Otto.

---
+++ src/main/java/com/parc/troy/SoarInterface.java
@@ -0,0 +1,148 @@
+package com.parc.troy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+import org.eclipse.jdt.annotation.NonNull;
+
+import com.parc.xi.dm.Config;
+import com.parc.xi.dm.LogicalForm;
+import com.parc.xi.dm.agent.DialogRuleFn;
+import com.parc.xi.dm.state.DialogState;
+import com.parc.xi.dm.state.Plan;
+
+import sml.Agent;
+import sml.Identifier;
+import sml.Kernel;
+import sml.sml;
+import sml.smlRunEventId;
+import sml.Agent.RunEventInterface;
+import sml.smlRhsEventId;
+
+
+public class SoarInterface implements DialogRuleFn, RunEventInterface {
+	private Kernel kernel;
+	private Agent troySoarAgent;
+	private String troySource;
+	private Identifier inputLink;
+	private Identifier outputLink;
+	private Identifier interactionLink;
+	
+	private boolean isRunning = false;
+	private boolean queueStop = false;
+	
+	private LogicalForm messageToWrite;
+
+	public SoarInterface(String name, String dmName){
+		this.configureAndStartSoarAgent(name, dmName);
+	}
+	
+	void configureAndStartSoarAgent(String name, String dmName)
+	{
+		this.kernel = Kernel.CreateKernelInNewThread();
+		this.troySoarAgent = kernel.CreateAgent(name);
+		
+		this.inputLink = troySoarAgent.GetInputLink();
+		this.outputLink = troySoarAgent.GetOutputLink();
+		this.interactionLink = this.inputLink.CreateIdWME("interaction");
+		
+		if (Config.getProperty(dmName + ".config.runType", null).equals("debug"))
+		{
+			troySoarAgent.SpawnDebugger(kernel.GetListenerPort());
+		}
+		String absoluteSoarRulesPath = Config.getProperty(dmName+".soarRules",null);
+		troySoarAgent.LoadProductions(absoluteSoarRulesPath);
+		this.start();
+		troySoarAgent.RegisterForRunEvent(smlRunEventId.smlEVENT_BEFORE_INPUT_PHASE, this, null);
+	}
+
+	@NonNull
+	public List<Result> apply(LogicalForm call, Plan plan, DialogState state) {
+		System.out.println("I am here");
+		List <Result> resultList = new ArrayList<Result>();
+		if (call.op.equals("writeToSoar")){
+			messageToWrite = call;
+			System.out.println("Command to soar sent out.");
+		}
+		
+		else{
+			new IllegalArgumentException("Unknown call: " + call.toString());
+		}
+		return resultList;
+	}
+	
+	public void start(){
+		if(isRunning){
+			return;
+		}
+    	class AgentThread implements Runnable{
+    		public void run(){
+    	    	isRunning = true;
+    			sendCommand("run");
+    			isRunning = false;
+    		}
+    	}
+    	Thread agentThread = new Thread(new AgentThread());
+    	agentThread.start();
+	}
+	
+	public void stop(){
+		queueStop = true;
+	}
+	
+	public String sendCommand(String command){
+		return troySoarAgent.ExecuteCommandLine(command);
+	}
+	
+	public void runEventHandler(int eventID, Object data, Agent agent, int phase) {
+		System.out.println("In Soar's running loop");
+		stopAgentIfRequested();
+		readOutputFromSoar();
+		writeInputToSoar();
+	}
+
+	private void stopAgentIfRequested() {
+		if(queueStop){
+			sendCommand("stop");
+			queueStop = false;
+		}
+	}
+	
+	private void writeInputToSoar() {
+		if (messageToWrite != null){
+			System.out.println("Write message to Soar in the running loop");
+			System.out.println(messageToWrite.getArg(0).toString());
+			if(messageToWrite.getArg(0).toString().equals("ActionCommand"))
+				writeActionCommand(messageToWrite.getArgList());
+			messageToWrite = null;
+		}
+	}
+	
+	private void writeActionCommand(List<LogicalForm> argList) {
+		Identifier commandId = this.interactionLink.CreateIdWME("action-command");
+		Iterator<LogicalForm> argListIterator = argList.iterator();
+		argListIterator.next();
+		commandId.CreateStringWME("verb", argListIterator.next().toString());
+		System.out.println("Wrote verb");
+		String baseObjectString = "object";
+		Integer i = 0;
+		while(argListIterator.hasNext()){
+			commandId.CreateStringWME(baseObjectString.concat(i.toString()), argListIterator.next().toString());
+			i++;
+			System.out.println("Wrote object.");
+		}
+		this.troySoarAgent.Commit();
+	}
+
+	private void readOutputFromSoar(){
+		
+	}
+	
+	
+	public String getErrorMessage(LogicalForm call) {
+		return null;
+	}
+	
+
+}
--- src/main/java/com/parc/troy/TroyAgent.java
+++ src/main/java/com/parc/troy/TroyAgent.java
@@ -11,52 +11,25 @@ import com.parc.xi.dm.agent.PlanStateAgent;
 import com.parc.xi.dm.agent.DialogRuleEngine;
 import com.parc.xi.dm.kb.KnowledgeBase;
 
-import sml.Agent;
-import sml.Kernel;
-import sml.smlRunEventId;
-import sml.Agent.RunEventInterface;
-import sml.smlRhsEventId;
 
-public class TroyAgent extends PlanStateAgent implements RunEventInterface{
+
+public class TroyAgent extends PlanStateAgent{
 	
-	private Kernel kernel;
-	private Agent troySoarAgent;
-	private String troySource;
-	
+	private SoarInterface soarInterface;
 	
 	public TroyAgent(String name, String dmName, KnowledgeBase kb){
 		this.name = LogicalFormConstants.AGENT;
 		if (name == null || name.isEmpty()) name = "troy";
-		if (dmName == null || dmName.isEmpty()) dmName = "troyDM";
-		
-		this.configureAndStartSoarAgent(dmName);
+		if (dmName == null || dmName.isEmpty()) dmName = "troyDM";	
+		this.soarInterface = new SoarInterface(name, dmName);
 		this.configureAndStartDialogEngine(dmName, kb);
 	}
-	
-	void configureAndStartSoarAgent(String dmName)
-	{
-		this.kernel = Kernel.CreateKernelInNewThread();
-		this.troySoarAgent = kernel.CreateAgent(this.name);
-		if (Config.getProperty(dmName + ".config.runType", null).equals("debug"))
-		{
-			troySoarAgent.SpawnDebugger(kernel.GetListenerPort());
-		}
-		String absoluteSoarRulesPath = Config.getProperty(dmName+".soarRules",null);
-		troySoarAgent.LoadProductions(absoluteSoarRulesPath);
-		troySoarAgent.RegisterForRunEvent(smlRunEventId.smlEVENT_AFTER_INPUT_PHASE, this, null);
-	}
-	
 	
 	private void configureAndStartDialogEngine(String dmName, KnowledgeBase kb)
 	{
 		engine = new DialogRuleEngine(name, dmName, kb);
+		engine.addAction("writeToSoar", this.soarInterface);
 		engine.readRules(Config.getProperty(dmName+".dialogRules",null));
 		addGenerator(engine);
 	}
-
-	public void runEventHandler(int arg0, Object arg1, Agent arg2, int arg3) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
