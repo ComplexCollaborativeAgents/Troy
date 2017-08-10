0a44322d9aed21c8056b7d59d2ad989681d1f4bb
smohan <shiwali.mohan@parc.com>
2017-08-10 14:13:44 -0700

    1. moved diagrams to src directory. 2. Implemented sub action elicitation

--- src/main/java/com/parc/troy/SoarInterface.java
+++ src/main/java/com/parc/troy/SoarInterface.java
@@ -147,7 +147,7 @@ public class SoarInterface implements DialogRuleFn, RunEventInterface {
 //			System.out.println(this.identifiersToRemove.size() + " identifiers to delete");
 //		}
 		for(Identifier id: this.getIdentifiersToRemove()){
-			System.out.println("Deleted: " + id.GetAttribute());
+			LOGGER.info("Deleted output command:" + id.GetAttribute());
 			id.AddStatusComplete();
 		}
 		this.identifiersToRemove.clear();
